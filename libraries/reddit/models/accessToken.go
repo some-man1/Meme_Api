e02a3a70f1fabfd4589e52dff988045c25317a09
some-man1 <142589483+some-man1@users.noreply.github.com>
2023-09-15 15:20:12 -0400

    Delete libraries directory

--- libraries/reddit/models/accessToken.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// AccessTokenBody : Model for Reddit AccessBody Response
-type AccessTokenBody struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	ExpiresIn   uint   `json:"expires_in"`
-	Scope       string `json:"scope"`
-}
--- libraries/reddit/models/customError.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// CustomRedditError : Custom Reddit Error
-type CustomRedditError struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
